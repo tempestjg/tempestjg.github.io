69c681e9c0bdfd8c3a701bcc393ce78963ec9949
Jose Garcia <tempestjg@yahoo.com>
2015-08-25 21:19:52 -0700

    asdf

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
   $.getJSON('example.json', function(data) {
-    console.log('getJSON test');
-    console.log(data);
+    console.log('got:', data);
+  })
+  .done(function(payload) {
+    console.log('done:', payload)
   })
   .fail(function(err) {
     console.log('failed:', err);
