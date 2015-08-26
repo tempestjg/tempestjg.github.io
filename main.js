2218efe639dc7b5a80e328271cbc640999d60028
Jose Garcia <tempestjg@yahoo.com>
2015-08-25 21:16:35 -0700

    hmm

--- main.js
+++ main.js
@@ -2,5 +2,8 @@ $(document).ready(function() {
   $.getJSON('example.json', function(data) {
     console.log('getJSON test');
     console.log(data);
+  })
+  .fail(function(err) {
+    console.log('failed:', err);
   });
 });
